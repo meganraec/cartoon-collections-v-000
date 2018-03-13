cc15c7bb990a4931e291cef8a8ab04cf42b7c561
Megan Collier <meganraec@live.com>
2018-03-13 04:06:29 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -15,6 +15,6 @@ end
 
 def find_the_cheese(list)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  list.detect {|i| i == cheddar_types.any?(i)}
+  list.detect {|i| i == cheddar_types.detect(i)}
   # list.detect {|cheese| cheese_types.any?{|cheese_type| cheese_type == cheese}}
 end
