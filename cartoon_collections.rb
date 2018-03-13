a380f616e50f217b3da40a288e9df7781065b83d
Megan Collier <meganraec@live.com>
2018-03-13 03:39:24 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,7 +10,7 @@ def summon_captain_planet(array)
 end
 
 def long_planeteer_calls(yodel)
-  yodel.map {|call| call.length >= 4}
+  yodel.detect {|call| call.length >= 4}
 end
 
 def find_the_cheese(list)
